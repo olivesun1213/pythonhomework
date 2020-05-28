b424c8666bb25cb22bb44f36caeff25a4fa19f4e
olivesun1213 <63970195+olivesun1213@users.noreply.github.com>
2020-05-28 16:03:10 -0400

    updates

--- Py_poll/main_py_poll.py
+++ Py_poll/main_py_poll.py
@@ -1,3 +1,4 @@
+f=open('poll_analysis.txt',"a")
 import os 
 import csv
 
@@ -8,13 +9,13 @@ with open(csvpath) as csvfile:
     # remove header line
     csv_header=next(csv_reader)
     list_of_rows=list(csv_reader)
-print("Election Results")
-print("-------------------------------")
+print("Election Results",file=f)
+print("-------------------------------",file=f)
 # find total number of month by counting the rows without header
 
 total_votes= len(list_of_rows)
-print("Total votes:" , total_votes)
-print("------------------------------")
+print("Total votes:" , total_votes,file=f)
+print("------------------------------",file=f)
 #create a new column called vote that transfer column C into String
 vote=[]
 for row in list_of_rows[2:]:
@@ -43,22 +44,23 @@ formatted_count_Corry="({})".format(count_Correy)
 formatted_count_Li="({})".format(count_Li)
 formatted_count_O="({})".format(count_O)
 
-print("Khan:", formatted_percent_khan, formatted_count_khan)
-print("Correy:", formatted_percent_Correy,formatted_count_Corry)
-print("Li:", formatted_percent_Li,formatted_count_Li)
-print("O'Tooley:",formatted_percent_O,formatted_count_O)
+print("Khan:", formatted_percent_khan, formatted_count_khan,file=f)
+print("Correy:", formatted_percent_Correy,formatted_count_Corry,file=f)
+print("Li:", formatted_percent_Li,formatted_count_Li,file=f)
+print("O'Tooley:",formatted_percent_O,formatted_count_O,file=f)
 
-print('------------------------------------------------')
+print('------------------------------------------------',file=f)
 
 # print the winner by looking at who has the largest number of votes
 if (count_Khan>count_Correy and count_Khan>count_Li and count_Khan>count_O):
-    print("Winner: Khan")
+    print("Winner: Khan",file=f)
 elif count_Correy > count_Khan and count_Correy > count_Li and count_Correy > count_O:
-    print("Winner:Correy")
+    print("Winner:Correy",file=f)
 elif count_Li>count_Khan and count_Li>count_Correy and count_Li>count_O:
-    print("Winner:Li")
+    print("Winner:Li",file=f)
 elif count_O > count_Khan and count_O > count_Correy and count_O >count_Li:
-    print("Winner: O'Tooley")
-print('-------------------------------------------------')
+    print("Winner: O'Tooley",file=f)
+print('-------------------------------------------------',file=f)
+f.close()
 
 
