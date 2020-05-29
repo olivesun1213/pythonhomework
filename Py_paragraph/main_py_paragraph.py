d9d47e5472368d1c2b9035e0ba1d0f93a55b65f4
olivesun1213 <63970195+olivesun1213@users.noreply.github.com>
2020-05-29 09:50:58 -0400

    updates

--- Py_paragraph/main_py_paragraph.py
+++ Py_paragraph/main_py_paragraph.py
@@ -1,17 +1,19 @@
-f=open('paragraph_analysis.txt',"a")
+#open analysis result as text
+g=open('paragraph_analysis.txt',"a")
 import os
-print("Paragraph Analysis",file=f)
-print("---------------------------",file=f)
+
 #import and read file
 textfile=os.path.join('.', 'Resources', 'paragraph.txt')
 f=open(textfile,'r')
-
+#print header
+print("Paragraph Analysis",file=g)
+print("---------------------------",file=g)
 # count number of words and number of sentences by counting space and .
 with open(textfile) as f:
     content=f.read()
     wordcount=len(content.split())
-    print('Approximate word conut:',str(wordcount),file=f)
-    print('Total Sentences:', content.count('.'))
+    print('Approximate word conut:',str(wordcount),file=g)
+    print('Total Sentences:', content.count('.'),file=g)
 
 # split paragraph into words
 words=content.split()
@@ -22,7 +24,7 @@ average_length=sum(lengths)/len(lengths)
 #format average length
 formatted_length_average="{:.1f}".format(average_length)
 
-print('Average letter count:', formatted_length_average,file=f )
+print('Average letter count:', formatted_length_average,file=g)
 countword=[]
 with open(textfile)as f:
     txt=f.read()
@@ -33,5 +35,5 @@ with open(textfile)as f:
 average_countword=sum(countword)/len(countword)
 #format to one decimal place
 formatted_average="{:.1f}".format(average_countword)
-print('Average Sentence Length:',formatted_average,file=f)
-f.close
+print('Average Sentence Length:',formatted_average,file=g)
+g.close()
